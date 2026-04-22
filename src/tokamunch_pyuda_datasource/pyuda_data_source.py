953a9e950189773d1ae235ecf815b8453cec276f
sdixon <stephen.dixon@ukaea.uk>
2026-04-22 13:12:21 +0100

    adding logging

--- src/tokamunch_pyuda_datasource/pyuda_data_source.py
+++ src/tokamunch_pyuda_datasource/pyuda_data_source.py
@@ -2,6 +2,11 @@ import pyuda
 import libtokamap
 import numpy as np
 from typing import override
+import logging
+import time
+
+
+logger = logging.getLogger(__name__)
 
 
 class PyudaDataSource(libtokamap.DataSource):
@@ -18,16 +23,41 @@ class PyudaDataSource(libtokamap.DataSource):
     @override
     def get(self, args: dict[str, str]) -> np.ndarray:
         if 'signal' not in args:
+            logging.error("signal is required")
             raise ValueError("signal is required")
         if 'source' not in args:
+            logging.error("source is required")
             raise ValueError("source is required")
         if 'host' not in args:
+            print("no host", flush=True)
+            logging.error("host is required")
             raise ValueError("host is required")
         if 'port' not in args:
+            logging.error("port is required")
             raise ValueError("port is required")
+
 
         plugin_args = [f'{k}=\"{v}\"' for (k, v) in args.items()]
         request = f'{self.plugin_name}::{self.function}({",".join(plugin_args)})'
 
+        logger.debug("Pyuda request: %s", request)
+
+        t0 = time.perf_counter()
         result = self.client.get(request, '')
+        dt = time.perf_counter() - t0
+
+        if result:
+            logger.debug(f"Pyuda result for signal {args['signal']}: \n"
+                         f"\tname: {result.label}\n"
+                         f"\ttype: {result.data.dtype}\n"
+                         f"\tshape: {result.data.shape}\n"
+                         f"\tsize: {result.data.size}\n"
+                         f"\tdims: {result.dims}\n"
+                         f"\ttime: {dt}")
+        else:
+            logger.debug(f"No data returned for signal {args['signal']}")
+
+        if "time" in args and args["time"]:
+            return np.asarray(result.time.data)
+
         return np.asarray(result.data)
